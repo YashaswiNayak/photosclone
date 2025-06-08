5c579c7cbbe91004fc1eb145b583f3f9e9601ccc
YashaswiNayak <123726092+YashaswiNayak@users.noreply.github.com>
2025-06-09 01:21:24 +0530

    Update PhotosController.java
    view photo by id

--- src/main/java/com/example/photosclone/PhotosController.java
+++ src/main/java/com/example/photosclone/PhotosController.java
@@ -1,14 +1,22 @@
 package com.example.photosclone;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
 public class PhotosController {
-
-    private List<Photo> db=List.of(new Photo("1","timepass.jpg"));
+    HashMap<String,Photo>db=new HashMap<>(){{
+        put("1",new Photo("1","timepass.jpg"));
+        put("2",new Photo("2","hello.jpg"));
+    }};
     @GetMapping("/")
     public  String defaultPage(){
         return "Default Page";
@@ -18,7 +26,15 @@ public class PhotosController {
         return "Hello World - I am Alive";
     }
     @GetMapping("/photos")
-    public List<Photo> getPhotos(){
-        return db;
+    public Collection<Photo> getAll(){
+        return db.values();
+    }
+    @GetMapping("/photos/{id}")
+    public Photo getPhoto(@PathVariable String id){
+        Photo photo= db.get(id);
+        if(photo==null){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return photo;
     }
 }
